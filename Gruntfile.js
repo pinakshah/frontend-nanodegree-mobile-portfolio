00999cc2c2c1f236173acf1b01d6951d2ae4516e
Pinak Shah <pinakshah33@gmail.com>
2016-03-24 17:59:11 +0530

    Changes for loading JS files asynchronously and load print css only for media type print

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function(grunt) {
         src: ['dist'],
       },
     },
-
     /* Generate the dist directory if it is missing */
     mkdir: {
       dev: {
@@ -19,18 +18,29 @@ module.exports = function(grunt) {
           create: ['dist']
         },
       },
-    }
+    },
+
+    /* Minify CSS files */
+    cssmin: {
+      target: {
+        files: [{
+          cwd: 'css',
+          src: ['*.css'],
+          dest: 'dist/css'
+        }]
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['clean', 'mkdir']);
+  grunt.registerTask('default', ['clean', 'mkdir', 'cssmin']);
 
 };
