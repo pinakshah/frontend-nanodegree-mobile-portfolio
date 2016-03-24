876b358630972936d240700477ba33eff96b873b
Pinak Shah <pinakshah33@gmail.com>
2016-03-24 18:03:38 +0530

    Revert cssmin changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,29 +18,18 @@ module.exports = function(grunt) {
           create: ['dist']
         },
       },
-    },
-
-    /* Minify CSS files */
-    cssmin: {
-      target: {
-        files: [{
-          cwd: 'css',
-          src: ['*.css'],
-          dest: 'dist/css'
-        }]
-      }
-    },
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['clean', 'mkdir', 'cssmin']);
+  grunt.registerTask('default', ['clean', 'mkdir']);
 
 };
