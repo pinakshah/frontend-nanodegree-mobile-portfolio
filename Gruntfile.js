8ccbedb838cf7437cf1a2dc346ced9420a371350
Pinak Shah <pinakshah33@gmail.com>
2016-03-25 19:58:55 +0530

    Update gruntfile - Add required plugins and tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,32 +4,70 @@
 
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
     /* Clear out the dist directory if it exists */
     clean: {
-      dev: {
+      dist: {
         src: ['dist'],
       },
     },
+
     /* Generate the dist directory if it is missing */
     mkdir: {
-      dev: {
+      dist: {
         options: {
           create: ['dist']
         },
       },
+    },
+
+    /* Minify HTML */
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**/*.html'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    /* Minify JS */
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/js/',
+          src: ['**/*.js'],
+          dest: 'dist/js/',
+          ext: '.min.js'
+        }]
+      }
+    },
+
+    /** Minify CSS **/
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css/',
+          ext: '.min.css'
+        }]
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['clean', 'mkdir']);
-
+  grunt.registerTask('default', ['clean', 'mkdir', 'htmlmin', 'uglify', 'cssmin']);
 };
+
